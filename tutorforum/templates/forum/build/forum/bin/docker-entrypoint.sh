67b639ca1a942cd7ef01accf86a9474804d24b22
Keith Grootboom <keith@opencraft.com>
2022-09-26 10:00:45 +0200

    feat: plugin now supports mongodb+srv urls and extra mongo config

--- tutorforum/templates/forum/build/forum/bin/docker-entrypoint.sh
+++ tutorforum/templates/forum/build/forum/bin/docker-entrypoint.sh
@@ -1,16 +1,19 @@
 #!/bin/sh -e
 
-export MONGOHQ_URL="mongodb://$MONGODB_AUTH$MONGODB_HOST:$MONGODB_PORT/$MONGODB_DATABASE"
 # the search server variable was renamed after the upgrade to elasticsearch 7
 export SEARCH_SERVER_ES7="$SEARCH_SERVER"
 
-# make sure that there is an actual authentication mechanism in place, if necessary
-if [ -n "$MONGODB_AUTH" ]
-then
-    export MONGOID_AUTH_MECH=":scram"
+echo "Waiting for mongodb/elasticsearch..."
+
+WAIT_MONGODB=""
+if [ $MONGODB_HOST = "mongodb+srv://*" ]; then
+    echo "MongoDB is using SRV records, so we cannot wait for it to be ready"
+    export MONGOHQ_URL="$MONGODB_HOST/$MONGODB_DATABASE"
+else
+    export MONGOHQ_URL="mongodb://$MONGODB_AUTH$MONGODB_HOST:$MONGODB_PORT/$MONGODB_DATABASE"
+    WAIT_MONGODB="-wait tcp://$MONGODB_HOST:$MONGODB_PORT"
 fi
 
-echo "Waiting for mongodb/elasticsearch..."
-dockerize -wait tcp://$MONGODB_HOST:$MONGODB_PORT -wait $SEARCH_SERVER -wait-retry-interval 5s -timeout 600s
+dockerize $WAIT_MONGODB -wait $SEARCH_SERVER -wait-retry-interval 5s -timeout 600s
 
 exec "$@"
