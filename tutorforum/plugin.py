20dfb980712000c5ce624e4689a17e30b4ea17fa
Régis Behmo <regis@behmo.com>
2023-03-20 14:13:16 +0100

    fix: don't crash on mongodb host querystring params
    When the MONGODB_HOST url contained querystring parameters, the forum
    was failing because the outdated mongodb gem does not support them. So
    we just trim them from the setting.
    
    Close #16.

--- tutorforum/plugin.py
+++ tutorforum/plugin.py
@@ -1,6 +1,7 @@
 from glob import glob
 import os
 import pkg_resources
+import urllib.parse
 
 from tutor import hooks as tutor_hooks
 
@@ -15,29 +16,38 @@ config = {
         "PORT": "4567",
         "API_KEY": "forumapikey",
         "REPOSITORY": "https://github.com/openedx/cs_comments_service.git",
-        "REPOSITORY_VERSION": "{{ OPENEDX_COMMON_VERSION }}"
+        "REPOSITORY_VERSION": "{{ OPENEDX_COMMON_VERSION }}",
     },
 }
 
-tutor_hooks.Filters.COMMANDS_INIT.add_item((
-    "forum",
-    ("forum", "tasks", "forum", "init"),
-))
+tutor_hooks.Filters.COMMANDS_INIT.add_item(
+    (
+        "forum",
+        ("forum", "tasks", "forum", "init"),
+    )
+)
 
-tutor_hooks.Filters.IMAGES_BUILD.add_item((
-    "forum",
-    ("plugins", "forum", "build", "forum"),
-    "{{ FORUM_DOCKER_IMAGE }}",
-    (),
-))
-tutor_hooks.Filters.IMAGES_PULL.add_item((
-    "forum",
-    "{{ FORUM_DOCKER_IMAGE }}",
-))
-tutor_hooks.Filters.IMAGES_PUSH.add_item((
-    "forum",
-    "{{ FORUM_DOCKER_IMAGE }}",
-))
+tutor_hooks.Filters.IMAGES_BUILD.add_item(
+    (
+        "forum",
+        ("plugins", "forum", "build", "forum"),
+        "{{ FORUM_DOCKER_IMAGE }}",
+        (),
+    )
+)
+tutor_hooks.Filters.IMAGES_PULL.add_item(
+    (
+        "forum",
+        "{{ FORUM_DOCKER_IMAGE }}",
+    )
+)
+tutor_hooks.Filters.IMAGES_PUSH.add_item(
+    (
+        "forum",
+        "{{ FORUM_DOCKER_IMAGE }}",
+    )
+)
+
 
 @tutor_hooks.Filters.COMPOSE_MOUNTS.add()
 def _mount_cs_comments_service(volumes, name):
@@ -53,10 +63,12 @@ def _mount_cs_comments_service(volumes, name):
         ]
     return volumes
 
+
 # Add the "templates" folder as a template root
 tutor_hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
     pkg_resources.resource_filename("tutorforum", "templates")
 )
+
 
 def auth_mech_as_ruby(auth_mech):
     """
@@ -75,11 +87,34 @@ def auth_mech_as_ruby(auth_mech):
         "SCRAM-SHA-1": ":scram",
         # SCRAM-256 is only supported from v2.6 of the ruby driver onwards.
         # See https://github.com/mongodb/mongo-ruby-driver/releases/tag/v2.6.0
-        "SCRAM-SHA-256": ":scram"
+        "SCRAM-SHA-256": ":scram",
     }.get(auth_mech) or ""
 
-tutor_hooks.Filters.ENV_TEMPLATE_FILTERS.add_item(
-    ("auth_mech_as_ruby", auth_mech_as_ruby)
+
+def forum_mongodb_host(host: str) -> str:
+    """
+    Remove the querystring parameters from the mongodb host url. These parameters are
+    not supported by the outdated mongodb gem. Thus we just trim them out.
+    """
+    # 0 = scheme
+    # 1 = netloc
+    # 2 = path
+    # 3 = params
+    # 4 = query
+    # 5 = fragment
+    parsed = [*urllib.parse.urlparse(host)]
+    parsed[4] = ""
+    # We also remove the trailing "/" from the path, which will not play well with the
+    # full url where we concatenate the database name.
+    parsed[2] = parsed[2].rstrip("/")
+    return urllib.parse.urlunparse(parsed)
+
+
+tutor_hooks.Filters.ENV_TEMPLATE_FILTERS.add_items(
+    [
+        ("auth_mech_as_ruby", auth_mech_as_ruby),
+        ("forum_mongodb_host", forum_mongodb_host),
+    ]
 )
 # Render the "build" and "apps" folders
 tutor_hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
@@ -96,12 +131,13 @@ for path in glob(
     )
 ):
     with open(path, encoding="utf-8") as patch_file:
-        tutor_hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
+        tutor_hooks.Filters.ENV_PATCHES.add_item(
+            (os.path.basename(path), patch_file.read())
+        )
 # Add configuration entries
 tutor_hooks.Filters.CONFIG_DEFAULTS.add_items(
-    [
-        (f"FORUM_{key}", value)
-        for key, value in config.get("defaults", {}).items()
-    ]
+    [(f"FORUM_{key}", value) for key, value in config.get("defaults", {}).items()]
 )
-tutor_hooks.Filters.CONFIG_OVERRIDES.add_items(list(config.get("overrides", {}).items()))
+tutor_hooks.Filters.CONFIG_OVERRIDES.add_items(
+    list(config.get("overrides", {}).items())
+)
