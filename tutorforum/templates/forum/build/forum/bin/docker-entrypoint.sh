a5e85e11f488ea4e7c0173c95f41c40a43635ead
Gabor Boros <gabor.brs@gmail.com>
2024-04-30 15:59:13 +0400

    feat: support ELASTICSEARCH_CA_PATH variable
    Signed-off-by: Gabor Boros <gabor.brs@gmail.com>

--- tutorforum/templates/forum/build/forum/bin/docker-entrypoint.sh
+++ tutorforum/templates/forum/build/forum/bin/docker-entrypoint.sh
@@ -5,15 +5,23 @@ export SEARCH_SERVER_ES7="$SEARCH_SERVER"
 
 echo "Waiting for mongodb/elasticsearch..."
 
+DOCKERIZE_COMMAND="dockerize -wait-retry-interval 5s -timeout 600s"
+
 WAIT_MONGODB=""
 if [ "${MONGODB_HOST#mongodb+srv://}" != "${MONGODB_HOST}" ]; then
     echo "MongoDB is using SRV records, so we cannot wait for it to be ready"
-    export MONGOHQ_URL="$MONGODB_HOST/$MONGODB_DATABASE"
+    export MONGOHQ_URL="${MONGODB_HOST}/${MONGODB_DATABASE}"
 else
-    export MONGOHQ_URL="mongodb://$MONGODB_AUTH$MONGODB_HOST:$MONGODB_PORT/$MONGODB_DATABASE"
-    WAIT_MONGODB="-wait tcp://$MONGODB_HOST:$MONGODB_PORT"
+    export MONGOHQ_URL="mongodb://${MONGODB_AUTH}${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE}"
+    WAIT_MONGODB="-wait tcp://${MONGODB_HOST}:${MONGODB_PORT}"
 fi
 
-dockerize $WAIT_MONGODB -wait $SEARCH_SERVER -wait-retry-interval 5s -timeout 600s
+WAIT_SEARCH_SERVER="-wait $SEARCH_SERVER"
+if [ -n "${ELASTICSEARCH_CA_PATH}" ]; then
+    WAIT_SEARCH_SERVER="${WAIT_SEARCH_SERVER} -cacert ${ELASTICSEARCH_CA_PATH}"
+fi
+
+$DOCKERIZE_COMMAND $WAIT_MONGODB
+$DOCKERIZE_COMMAND $WAIT_SEARCH_SERVER
 
 exec "$@"
